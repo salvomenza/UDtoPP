f6899f9ed5a9a2b9bac89bb980a42b3aea373786
salvomenza <salvomenza@gmail.com>
2026-03-31 23:35:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# ver. 26
+# ver. 26.1
 """
 app.py
 Interfaccia web Flask per il generatore di alberi chomskiani.
@@ -9,7 +9,7 @@ import requests
 import json
 from datetime import datetime
 
-VERSION = "0.26"
+VERSION = "0.26.1"
 BUILD_DATE = datetime.now().strftime("%d/%m/%Y")
 BUILD_TIME = datetime.now().strftime("%H:%M")
 from test_conllu import parse_conllu
